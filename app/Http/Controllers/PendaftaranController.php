ce03cbf2464a2b08c28af104d36027ef52b27e16
Tomi2903 <tomi085755@gmail.com>
2025-01-12 12:44:08 +0700

    baru

--- app/Http/Controllers/PendaftaranController.php
+++ app/Http/Controllers/PendaftaranController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Pendaftarans;
 
-class PendaftaranTables extends Controller
+class PendaftaranController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -18,6 +18,7 @@ class PendaftaranTables extends Controller
         $pendaftaran = Pendaftarans::all();
         return view('public.pages.informasi', compact('pendaftaran'));
     }
+
 
     /**
      * Show the form for creating a new resource.
--- app/Http/Controllers/PenerimaanController.php
+++ app/Http/Controllers/PenerimaanController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Models\Penerimaans;
 
-class penerimaan extends Controller
+class penerimaanController extends Controller
 {
 /**
      * Display a listing of the resource.
